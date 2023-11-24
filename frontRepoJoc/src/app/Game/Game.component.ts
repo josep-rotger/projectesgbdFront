ab8bd6a5ae7d2e1467ebc180d6f48aabd22a39ff
David Bouzas <u1972821@campus.udg.edu>
2023-11-24 18:59:29 +0100

    Filtres individuals

--- frontRepoJoc/src/app/Game/Game.component.ts
+++ frontRepoJoc/src/app/Game/Game.component.ts
@@ -13,6 +13,16 @@ export class GameComponent implements OnInit{
   
   games: Game[] = [];
   reviews: Review[] = [];
+  searchCompany: string = '';
+  searchRating: number = -1;
+  searchPrice: number = -1;
+  stars = [
+    { value: 1, active: false },
+    { value: 2, active: false },
+    { value: 3, active: false },
+    { value: 4, active: false },
+    { value: 5, active: false }
+  ];
 
   constructor(private gameService: GameService, private router: Router) {}
 
@@ -26,6 +36,51 @@ export class GameComponent implements OnInit{
         console.error('Error al obtenir dades del joc:', error);
       }
     );
+  }
+
+
+  toggleStar(star: any): void {
+    const starIndex = this.stars.indexOf(star);
+    this.searchRating= 0;
+
+    // desmarca les estrelles que hi havia abans
+    for (let i = 0; i < this.stars.length; i++) {
+      this.stars[i].active = false;
+    }
+
+    // marca les estrelles
+    for (let i = 0; i <= starIndex; i++) {
+      this.stars[i].active = !this.stars[i].active;
+      this.searchRating+=1;
+    }
+
+    this.applyFilters();
+  }
+
+  applyFilters(): void {
+
+    this.searchGames('Rating', this.searchRating.toString());
+
+    this.searchGames('Company', this.searchCompany);
+
+    this.searchGames('Price', this.searchPrice.toString());
+
+  }
+
+  searchGames(field: string, value: string): void {
+    this.gameService.searchByFieldAndValue(field, value).subscribe(
+      (jocIterable: Iterable<Game>) => {
+        // convertir un iterable a un array
+        this.games = Array.from(jocIterable);
+      },
+      error => {
+        console.error('Error al buscar jocs:', error);
+      }
+    );
+  }
+
+  loadGames() {
+    throw new Error('Method not implemented.');
   }
 
   navigateToGameDetails(gameId: number): void {
