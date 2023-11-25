ecc2a5d1ee2ad86fa2ef5a03e10ff52e5f19e333
Josep <r.g.jos2001@gmail.com>
2023-11-25 10:30:44 +0100

    canvis generals

--- frontRepoJoc/src/app/app.module.ts
+++ frontRepoJoc/src/app/app.module.ts
@@ -12,11 +12,10 @@ import { ReviewComponent } from './review/review.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddReviewModalComponent } from './add-review-modal/add-review-modal.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
